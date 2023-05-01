94f9c7acb4eb70f40f2fdda6dd981b1ad6c4e447
Fatima Ali <fofoalmali@gmail.com>
2023-05-01 21:46:37 +0300

    Assignment done

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function Driver(name, age, experience){
     this.experience=experience;
 }
 let allison = new Driver("Allison", "16 years", "2 years");
+
+function PickupLocation(address, city){
+    this.address=address;
+    this.city=city;
+}
+let poshHotel = new PickupLocation("123 Broadway", "New York City");
