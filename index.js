5a90522ddd3a4e2fb27a7af903b0580bc5ed7d87
Fatima Ali <fofoalmali@gmail.com>
2023-05-01 21:43:52 +0300

    Driver Done

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ function Scooter(year, color, model){
     this.color=color;
     this.model=model;
 }
-
 const samson=new Scooter(2000,"red","swoosh");
+
+function Driver(name, age, experience){
+    this.name=name;
+    this.age=age;
+    this.experience=experience;
+}
+let allison = new Driver("Allison", "16 years", "2 years");
